0abae0542f579c3d87e08d63fa58ee79d8518b8f
rahverma1990 <rahverma@tibco.com>
2017-05-23 16:46:11 +0530

    fixed error.

--- karma.conf.js
+++ karma.conf.js
@@ -48,6 +48,6 @@ module.exports = function (config) {
     singleRun: false
   });
   if(process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci'];
+    config.browsers = ['Chrome_travis_ci'];
   }
 };
